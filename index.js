3f3b50daf7e91f808c771b127f7efe1e0bb658af
huynh nha tuan <huynhnhatuanqnu@gmail.com>
2019-05-14 21:53:48 +0700

    add 3 files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-Hello, welcome to Nodejs programming
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to Nodejs programming');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+-- learning to Express application generator
+https://expressjs.com/en/starter/generator.html
+http://atmarkcafe.org/tieng-viet-viet-responsive-web-voi-skeleton/?lang=vi
