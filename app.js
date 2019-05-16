73b0a86eb4419c0a67ddae53ddf59536886fc69c
huynh nha tuan <huynhnhatuanqnu@gmail.com>
2019-05-16 23:05:03 +0700

    commit 3

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 // Define Dependences
 const express = require('express');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 // Content
 const app = express();
 
+app.use(logger('dev'));
+
 /** Set up bodyparser */
 app.set(bodyParser.urlencoded({ extended: false }));
 app.set(bodyParser.json());
+
+app.use('/', (req, res) => {
+    res.status(200).json({
+        message: 'URL is correct!'
+    });
+});
+
+app.use((req, res, next) => {
+    // res.status(404).json({
+    //     message: 'URL not found!'
+    // });
+    // next(res);
+});
 
 
 // Module exports
